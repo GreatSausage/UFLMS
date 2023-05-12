366006b62f684e65da897af75c8c2a531a11ee5f
GreatSausage <112478992+GreatSausage@users.noreply.github.com>
2023-05-12 18:03:22 +0800

    created an executed file

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UFLMS.FrmMain
+            Me.MainForm = Global.UFLMS.FrmSignInScreen
         End Sub
     End Class
 End Namespace
